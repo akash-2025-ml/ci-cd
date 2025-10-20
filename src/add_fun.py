a2675239cabd946f7a0dad56ad12f3dbc6332bbb
AkashAaglawe22012 <akashaaglawe0@gmail.com>
2025-10-20 20:09:31 +0530

    second

--- src/add_fun.py
+++ src/add_fun.py
@@ -4,3 +4,7 @@ def add(a, b):
 
 def sub(a, b):
     return a - b
+
+
+def ret(a):
+    return a
--- test/test_operations.py
+++ test/test_operations.py
@@ -1,11 +1,22 @@
-from src.add_fun import add, sub
+from src.add_fun import add, sub, ret
 
 
 def test_add():
     assert add(2, 3) == 5
+    assert add(3, 3) == 6
+    assert add(8, 2) == 10
+    assert add(3, 7) == 10
     assert add(3, 3) == 6
 
 
 def test_sub():
     assert sub(6, 2) == 4
     assert sub(3, 3) == 0
+
+
+def test_ret():
+    assert ret("sayjvsf") == "sayjvsf"
+    assert ret("kdshcvd") == "kdshcvd"
+    assert ret("sayjvsf") == "sayjvsf"
+    assert ret("sayjvsf") == "sayjvsf"
+    assert ret("sayjvsf") == "sayjvsf"
